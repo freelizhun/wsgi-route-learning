c7f8e8f3446c7753f3ee616ad54a50423ee89a69
lizhun <lizhun@kylinos.cn>
2021-01-26 20:42:27 +0800

    新增uwsgi启动服务的支持

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 Example of the route and wsgi for http server
 =========
-学习route与wsgi的小用例
+##学习route与wsgi的小用例
+###Get Started
+```bash
+$ git clone https://github.com/freelizhun/wsgi-route-learning.git
 
+$ cd wsgi-route-learning
 
+$ git checkout with-uwsgi
+
+$ virtualenv venv
+
+$ source venv/bin/activate
+
+$ pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requirements.txt 
+```
+###用uwsgi从后台启动并写入到日志文件
+```bash
+$ uwsgi --yaml uwsgi_test.yml --daemonize /tmp/my_uwsgi.log
+```
+浏览器输入http://192.168.108.131:8080/user/login观察测试结果
+###关闭与重启uwsgi
+```bash
+$ uwsgi --stop uwsgi/uwsgi.pid
+$ uwsgi --reload uwsgi/uwsgi.pid
+```
--- service/ps_launcher.py
+++ service/ps_launcher.py
@@ -6,28 +6,7 @@ def make_app(mode):
     service_app.add_route("/user/login", 'UserApp', 'login')
     return service_app
 
-def main():
-    import optparse
-    parser = optparse.OptionParser(
-        usage="%prog [OPTIONS] MODULE:EXPRESSION")
-    parser.add_option(
-        '-P', '--port', default='8080',
-        help='Port to serve on (default 8080)')
-    parser.add_option(
-        '-H', '--host', default='0.0.0.0',
-        help='Host to serve on (default localhost; 0.0.0.0 to make public)')
-    parser.add_option(
-        '-M', '--mode', default='debug',
-        help='debug or prod mode')
-    options, args_ = parser.parse_args()
 
-    entry_app = make_app(options.mode)
-
-    if options.mode == 'debug' or 'reset':
-        options.port = 8002
-
-    from paste import httpserver
-    httpserver.serve(entry_app,host=options.host,port=options.port)
-
-if __name__ == '__main__':
-    main()
+def uwsgi_app():
+    mode="debug"
+    return make_app(mode)
